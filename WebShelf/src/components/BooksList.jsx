e89ffb58bdfaee7b3c76045f9a66043cc88c9bcd
Guilherme Fernandes <84087774+Gui28F@users.noreply.github.com>
2023-11-11 16:20:19 +0000

    Finished BrowseAll

--- WebShelf/src/components/BooksList.jsx
+++ WebShelf/src/components/BooksList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./BooksList.css"
+import StarRateIcon from '@mui/icons-material/StarRate';
+import {Icon} from "@mui/material";
 const BooksList = (props) => {
     return (
         <div className="books_container">
@@ -7,8 +9,12 @@ const BooksList = (props) => {
                 <div key={index} className="book-container">
                 <div  className='image-container'>
                     <img src={book.imageUrl} alt='book'></img>
+                    <div className="rating">{book.rating}
+                        <StarRateIcon className="star_rating"/>
+                    </div>
                 </div>
                 <div className="book-title">{book.title}</div>
+
                 </div>
             ))}
         </div>

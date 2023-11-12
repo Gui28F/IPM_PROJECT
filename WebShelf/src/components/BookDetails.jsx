4509e54fdcec42d2f9ff0d3a5451bc86aa1ba08d
Guilherme Fernandes <84087774+Gui28F@users.noreply.github.com>
2023-11-12 18:07:43 +0000

    Added icons in BookDetails

--- WebShelf/src/components/BookDetails.jsx
+++ WebShelf/src/components/BookDetails.jsx
@@ -1,9 +1,28 @@
 // BookDetails.jsx
 import React, { useEffect, useState } from 'react';
-import {useParams} from "react-router-dom";
+import {useLocation, useParams} from "react-router-dom";
 import Data from "./Data.jsx";
 import "./BookDetails.css"
+import bookMark from "../assets/book_mark_white.svg";
+import bookMarkTicked from "../assets/book_mark_ticked.svg";
+import favorite from "../assets/favorite_white.svg";
+import favoriteTicked from "../assets/favorite_ticked.svg";
+import {Rating} from "@mui/material";
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 const BookDetails = (props) => {
+    const location = useLocation();
+
+    const [favTicked, setFavTicked] = useState(false);
+    const [bookmarkTicked, setBookmarkTicked] = useState(false);
+    const [rating, setRating] = useState(0);
+
+    const toggleFavorite = () => {
+        setFavTicked((favTicked) => !favTicked);
+    };
+
+    const toggleBookmark = () => {
+        setBookmarkTicked((bookmarkTicked) => !bookmarkTicked);
+    };
     const params = useParams();
     const bookId = parseInt(params.id, 10); // Convert id to integer
     const book = Data.find(book => book.id === bookId);
@@ -11,9 +30,26 @@ const BookDetails = (props) => {
         <div className="indv_out-container">
             <h1 className="indv_book-title">{book.title}</h1>
             <div className="indv_details-container">
-                <img className="indv_image-container" src={book.imageUrl} alt='book'></img>
+                <div className='indv_image-container'>
+                    <img id="book-img" src={book.imageUrl} alt='book'></img>
+                    <div className="indv_icons">
+                        {!bookmarkTicked && (<img className='indv_book_mark_ico' alt="favourites" src={bookMark} onClick={toggleBookmark}></img>)}
+                        {bookmarkTicked && (<img className='indv_book_mark_ico' alt="favourites" src={bookMarkTicked} onClick={toggleBookmark}></img>)}
+                        {!favTicked && (<img className='indv_favorite_ico' src={favorite} onClick={toggleFavorite}></img>)}
+                        {favTicked && (<img className='indv_favorite_ico' src={favoriteTicked} onClick={toggleFavorite}></img>)}
+                        <Rating
+                            className="indv_rating_stars"
+                            name="simple-controlled"
+                            value={rating}
+                            onChange={(event, newValue) => {
+                                setRating(newValue);
+                            }}
+                            emptyIcon={<StarBorderIcon style={{ color: "white" }} fontSize="inherit"  />}
+                        />
+                    </div>
+                </div>
                 <div className="indv_synopsis">
-                <div >
+                <div className="indv_synopsis_text">
                     {book.synopsis}
                 </div>
                     <div className="indv_genres-list">
@@ -22,8 +58,11 @@ const BookDetails = (props) => {
                                 {genre}
                              </span>
                         ))}
+                    </div>
                 </div>
-                </div>
+                <button className="indv_add_to_shelf" onClick={() => console.log("Button clicked!")}>
+                    Add to Shelf
+                </button>
             </div>
             {/* Display other book details here */}
         </div>

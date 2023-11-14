950b9af9eb2f409c2c69386b6a3023d705bbf6e9
vladbug <v.mikytiv@campus.fct.unl.pt>
2023-11-14 12:02:29 +0000

    Help me

--- WebShelf/src/components/MonthlySuggestions.jsx
+++ WebShelf/src/components/MonthlySuggestions.jsx
@@ -5,6 +5,7 @@ import $ from 'jquery';
 import {Books} from "./month.js"
 
 const MonthlySuggestions = () => {
+  
     const $books = $('#bk-list > li > div.bk-book');
     const booksCount = $books.length;
   
@@ -61,8 +62,8 @@ const MonthlySuggestions = () => {
         });
   
         if ($content.length > 1) {
-          const $navPrev = $('<span class="bk-page-prev">&lt;</span>');
-          const $navNext = $('<span class="bk-page-next">&gt;</span>');
+          const $navPrev = $('<span className="bk-page-prev">&lt;</span>');
+          const $navNext = $('<span className="bk-page-next">&gt;</span>');
   
           $page.append($('<nav></nav>').append($navPrev, $navNext));
   
@@ -93,7 +94,7 @@ const MonthlySuggestions = () => {
     }, []);
   
     return (
-        <html lang="en" className="no-js">
+        
         <div className="container">
 	<div className="main">
 		<ul id="bk-list" className="bk-list clearfix">
@@ -108,37 +109,37 @@ const MonthlySuggestions = () => {
 									</h2>
 						</div>
 					</div>
-					<div class="bk-page">
-						<div class="bk-content bk-content-current">
+					<div className="bk-page">
+						<div className="bk-content bk-content-current">
 							<p>Red snapper Kafue pike fangtooth humums slipmouth, salmon cutlassfish; swallower European perch mola mola sunfish, threadfin bream. Billfish hog sucker trout-perch lenok orbicular velvetfish. Delta smelt striped bass, medusafish dragon goby starry
 								flounder cuchia round whitefish northern anchovy spadefish merluccid hake cat shark Black pickerel. Pacific cod.</p>
 						</div>
-						<div class="bk-content">
+						<div className="bk-content">
 							<p>Whale catfish leatherjacket deep sea anglerfish grenadier sawfish pompano dolphinfish carp large-eye bream, squeaker amago. Sandroller; rough scad, tiger shovelnose catfish snubnose parasitic eel? Black bass soldierfish duckbill--Rattail Atlantic
 								saury Blind shark California halibut; false trevally warty angler!</p>
 						</div>
-						<div class="bk-content">
+						<div className="bk-content">
 							<p>Trahira giant wels cutlassfish snapper koi blackchin mummichog mustard eel rock bass whiff murray cod. Bigmouth buffalo ling cod giant wels, sauger pink salmon. Clingfish luderick treefish flatfish Cherubfish oldwife Indian mul gizzard shad hagfish
 								zebra danio. Butterfly ray lizardfish ponyfish muskellunge Long-finned sand diver mullet swordfish limia ghost carp filefish.</p>
 						</div>
-						<nav><span class="bk-page-prev">&lt;</span><span class="bk-page-next">&gt;</span></nav>
+						<nav><span className="bk-page-prev">&lt;</span><span className="bk-page-next">&gt;</span></nav>
 					</div>
-					<div class="bk-back">
+					<div className="bk-back">
 						<p>In this nightmare vision of cats in revolt, fifteen-year-old Alex and his friends set out on a diabolical orgy of robbery, rape, torture and murder. Alex is jailed for his teenage delinquency and the State tries to reform him - but at what cost?</p>
 					</div>
-					<div class="bk-right"></div>
-					<div class="bk-left">
+					<div className="bk-right"></div>
+					<div className="bk-left">
 						<h2>
 									<span>Anthony Burghiss</span>
 									<span>A Catwork Orange</span>
 								</h2>
 					</div>
-					<div class="bk-top"></div>
-					<div class="bk-bottom"></div>
+					<div className="bk-top"></div>
+					<div className="bk-bottom"></div>
 				</div>
-				<div class="bk-info">
-					<button class="bk-bookback">Flip</button>
-					<button class="bk-bookview">View inside</button>
+				<div className="bk-info">
+					<button className="bk-bookback">Flip</button>
+					<button className="bk-bookview">View inside</button>
 					<h3>
 								<span>Anthony Burghiss</span>
 								<span>A Catwork Orange</span>
@@ -149,7 +150,6 @@ const MonthlySuggestions = () => {
 		</ul>
 	</div>
 </div>
-      </html>
     );
 };
 

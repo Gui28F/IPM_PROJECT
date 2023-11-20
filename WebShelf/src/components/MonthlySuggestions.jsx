277fba483459888ba0e01ccd70f54926f8c03224
Guilherme Fernandes <84087774+Gui28F@users.noreply.github.com>
2023-11-20 19:02:03 +0000

    Update MonthlySuggestions.jsx

--- WebShelf/src/components/MonthlySuggestions.jsx
+++ WebShelf/src/components/MonthlySuggestions.jsx
@@ -158,7 +158,7 @@ export default function Shelves() {
           </button>
         ))}
       </div>
-      <div className="flex justify-between mt-4" style={{ transform: 'translateY(50%)' }}>
+      <div className="flex justify-between mt-4" style={{ transform: 'translateY(50vh)' }}>
         <button onClick={handlePrevPage} className="px-2 py-2 ml-4 bg-gray-800 text-white">
           <ChevronLeftIcon className="h-6 w-6" /> {/* Left Arrow Icon */}
         </button>

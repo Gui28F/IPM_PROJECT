a931fa0081410e4af36e61e5289ec4325ac4e5d8
vladys <v.mikytiv@campus.fct.unl.pt>
2023-11-20 19:20:39 +0000

    Margins Fixed

--- WebShelf/src/components/MonthlySuggestions.jsx
+++ WebShelf/src/components/MonthlySuggestions.jsx
@@ -158,11 +158,11 @@ export default function Shelves() {
           </button>
         ))}
       </div>
-      <div className="flex justify-between mt-4" style={{ transform: 'translateY(50%)' }}>
-        <button onClick={handlePrevPage} className="px-2 py-2 ml-4 bg-gray-800 text-white">
+      <div className="flex justify-between mt-4" style={{ transform: 'translateY(25rem)' }}>
+        <button onClick={handlePrevPage} className="px-2 py-2 ml-20 bg-gray-800 text-white"  style={{ marginLeft: '650px' }}>
           <ChevronLeftIcon className="h-6 w-6" /> {/* Left Arrow Icon */}
         </button>
-        <button onClick={handleNextPage} className="px-2 py-2 mr-4 bg-gray-800 text-white">
+        <button onClick={handleNextPage} className="px-2 py-2 mr-20 bg-gray-800 text-white" style={{ marginRight: '650px' }}>
           <ChevronRightIcon className="h-6 w-6" /> {/* Right Arrow Icon */}
         </button>
       </div>
--- WebShelf/src/components/Shelf.jsx
+++ WebShelf/src/components/Shelf.jsx
@@ -243,18 +243,20 @@ export default function MonthlySuggestions() {
             </div>
             <div
                 className="flex justify-between mt-4"
-                style={{ transform: "translateY(25.5rem)" }}
+                style={{ transform: "translateY(17.5rem)" }}
             >
                 <button
                     onClick={handlePrevPage}
-                    className="px-2 py-2 ml-4 bg-gray-800 text-white"
+                    className="px-2 py-2 ml-20 bg-gray-800 text-white"
+                    style={{ marginLeft: '650px' }}
                 >
                     <ChevronLeftIcon className="h-6 w-6" />{" "}
                     {/* Left Arrow Icon */}
                 </button>
                 <button
                     onClick={handleNextPage}
-                    className="px-2 py-2 mr-4 bg-gray-800 text-white"
+                    className="px-2 py-2 mr-20 bg-gray-800 text-white"
+                    style={{ marginRight: '650px' }}
                 >
                     <ChevronRightIcon className="h-6 w-6" />{" "}
                     {/* Right Arrow Icon */}

bf1cf6f8ebf517c9c5dcc165d7b2838396196b41
vladys <v.mikytiv@campus.fct.unl.pt>
2023-11-26 21:33:53 +0000

    Ability to add shelves in My Shelves

--- WebShelf/src/components/MyShelves.jsx
+++ WebShelf/src/components/MyShelves.jsx
@@ -1,17 +1,106 @@
 // MyShelves.jsx
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MyShelves.css";
 import { users } from "./Data.jsx";
 import { Link } from "react-router-dom";
-
-
+import { Box, Checkbox, Modal, Rating, Typography, Popover, Button } from "@mui/material";
+import { createTheme } from '@mui/material/styles';
+import AddIcon from '@mui/icons-material/Add';
+import CloseIcon from '@mui/icons-material/Close';
 const MyShelves = () => {
+
     const handleShelfClick = (id) => {
         console.log(`Shelf ${id} clicked`);
     };
 
+    const theme = createTheme({
+        palette: {
+            purple: {
+            main: '#2F334B',
+            light: '#AFA2FF',
+            dark: '#101119',
+            contrastText: '#ffffff',
+            },
+        },
+        });
+
     const userShelves = users[0].shelves;
+    const currentUser = users[0];
+    const [selectedShelves, setSelectedShelves] = useState([]);
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const [newShelfName, setNewShelfName] = useState("");
+    const [customTicked, setCustomTicked] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    const handleSubmit = () => {
+        selectedShelves.forEach((selectedShelf) => {
+            
+                console.log("entrei aquii")
+                // Shelf does not exist, create a new one and add the book
+                const newShelfID = users[0].shelves.reduce((maxID, shelf) => Math.max(shelf.id, maxID), 0) + 1;
+                const newShelf = {
+                    id: newShelfID,
+                    name: selectedShelf,
+                    books: [],
+                    booksIDs: [],
+                };
+                users[0].shelves = [...users[0].shelves, newShelf];
+            
+        });
+
+        setSuccess(true);
+        if (selectedShelves.includes("To Read") && !bookmarkTicked)
+            setBookmarkTicked(true);
+        if (selectedShelves.includes("Favourites") && !favTicked)
+            setFavTicked(true);
+    
+        setTimeout(() => {
+            handleClose();
+            setSuccess(false);
+            setNewShelfName(""); // Clear the old shelf name
+        }, 1000); // 1000 milliseconds = 1 second
+    };
+
+    const handleShelfNameChange = (newName) => {
+        // Update the customTicked state based on whether the shelf name is not empty
+        setCustomTicked(newName !== "");
+
+        // Update the selectedShelves state with only the latest value
+        setSelectedShelves((prevSelected) => {
+            // Check if the new name already exists in the selected shelves
+            if (prevSelected.includes(newName)) {
+                // If it does, return the array unchanged
+                return prevSelected;
+            }
+
+            // Filter out only the exact match of the new name
+            const filteredShelves = prevSelected.filter((shelf) => shelf !== newShelfName);
+
+            // Add the new name to the filtered shelves only if it does not already exist
+            if (!filteredShelves.includes(newName)) {
+                return [...filteredShelves, newName];
+            }
+
+            // If the new name already exists, return the filtered shelves unchanged
+            return filteredShelves;
+        });
+
+        // Update the new shelf name
+        console.log(newName);
+        setNewShelfName(newName);
+    };
+
+    const handleShelfToggle = (shelf) => {
+        setSelectedShelves((prevSelected) => {
+            if (prevSelected.includes(shelf)) {
+                return prevSelected.filter((selected) => selected !== shelf);
+            } else {
+                return [...prevSelected, shelf];
+            }
+        });
+    };
 
     return (
         <div className="my-shelves-container">
@@ -30,8 +119,86 @@ const MyShelves = () => {
                     </div>
                 ))}
             </div>
+
+            <div className="indv_add_to_shelf">
+                        <Button  theme={theme} onClick={handleOpen} variant="contained" color="purple"  size="large" startIcon={<AddIcon/>}
+                        style={{ position: "fixed", bottom: "20px", left: "50%", transform: "translateX(-50%)" }}>
+                            Add New Shelf
+                        </Button>
+            </div>
+            <Modal
+                        open={open}
+                        onClose={() => handleClose()}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box closeButton className={"modal-box"}>
+                            <Typography
+                                className="modal-modal-title"
+                                variant="h6"
+                                component="h2"
+                            >
+                                Shelves
+                                <CloseIcon className="close-icon-details" onClick={handleClose}></CloseIcon>
+
+                            </Typography>
+
+                            {currentUser.shelves.map((shelf) => (
+                                <div key={shelf.name} className="indv_shelf_row">
+                                    <Checkbox
+                                        checked={selectedShelves.includes(
+                                            shelf.name
+                                        )}
+                                        onChange={() =>
+                                            handleShelfToggle(shelf.name)
+                                        }
+                                        color="primary"
+                                    />
+                                    <span className="indv_shelf_name">
+                                        {shelf.name}
+                                    </span>
+                                </div>
+                            ))}
+                            <div className="indv_add_shelf_container">
+                                <Checkbox
+                                    checked={selectedShelves.includes(newShelfName) || customTicked}
+                                    onChange={() => {
+                                        setSelectedShelves((prevSelected) =>
+                                            prevSelected.includes(newShelfName)
+                                                ? prevSelected.filter(
+                                                    (selected) =>
+                                                        selected !== newShelfName
+                                                )
+                                                : [...prevSelected, newShelfName]
+                                        );
+                                    }}
+                                    color="primary"
+                                />
+                                <input
+                                    className="indv_new_shelf_input"
+                                    type="text"
+                                    placeholder="New Shelf"
+                                    value={newShelfName}
+                                    onChange={(e) =>
+                                        handleShelfNameChange(e.target.value)
+                                    }
+                                />
+                            </div>
+                            <button
+                                className={`indv_submit_button${success ? "_success" : ""
+                                    }`}
+                                onClick={handleSubmit}
+                            >
+                                {success ? "Saved" : "Save"}
+                            </button>
+                        </Box>
+                    </Modal>
         </div>
+        
+        
+        
     );
 };
+
 
 export default MyShelves;

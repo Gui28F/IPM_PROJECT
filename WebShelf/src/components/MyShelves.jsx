95c8b8afd4b19cdaa1b200aa41498152128b0b68
Guilherme Fernandes <84087774+Gui28F@users.noreply.github.com>
2023-11-27 09:51:30 +0000

    add delete and edit in shelves

--- WebShelf/src/components/MyShelves.jsx
+++ WebShelf/src/components/MyShelves.jsx
@@ -1,12 +1,14 @@
 // MyShelves.jsx
-import React, { useEffect, useState } from "react";
+import React, {useState} from "react";
 import "./MyShelves.css";
-import { users } from "./Data.jsx";
-import { Link } from "react-router-dom";
-import { Box, Checkbox, Modal, Rating, Typography, Popover, Button } from "@mui/material";
-import { createTheme } from '@mui/material/styles';
+import {users} from "./Data.jsx";
+import {Link} from "react-router-dom";
+import {Box, Button, Modal, Typography} from "@mui/material";
+import {createTheme} from '@mui/material/styles';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 const MyShelves = () => {
 
     const handleShelfClick = (id) => {
@@ -24,21 +26,27 @@ const MyShelves = () => {
         },
         });
 
-    const userShelves = users[0].shelves;
+    const [userShelves, setUserShelves] = useState(users[0].shelves);
     const currentUser = users[0];
     const [selectedShelves, setSelectedShelves] = useState([]);
     const [open, setOpen] = React.useState(false);
+    const [editOpen, setEditOpen] = React.useState(false);
+    const [editedName, setEditedName] = useState("");
     const handleOpen = () => setOpen(true);
+    const handleEditOpen = () => setEditOpen(true);
     const handleClose = () => setOpen(false);
+    const handleEditClose = () => {
+        setEditOpen(false);
+    }
     const [newShelfName, setNewShelfName] = useState("");
+    const [updatedShelfName, updateShelfName] = useState("");
     const [customTicked, setCustomTicked] = useState(false);
     const [success, setSuccess] = useState(false);
 
     const handleSubmit = () => {
         let added = false;
         selectedShelves.forEach((selectedShelf) => {
-                
-            console.log(selectedShelf);
+
             const existingShelf = currentUser.shelves.find(
                 (shelf) => shelf.name === selectedShelf
             );
@@ -48,7 +56,6 @@ const MyShelves = () => {
             } 
 
             else {
-                console.log("entrei aquii")
                 // Shelf does not exist, create a new one and add the book
                 const newShelfID = users[0].shelves.reduce((maxID, shelf) => Math.max(shelf.id, maxID), 0) + 1;
                 const newShelf = {
@@ -58,6 +65,7 @@ const MyShelves = () => {
                     booksIDs: [],
                 };
                 users[0].shelves = [...users[0].shelves, newShelf];
+                setUserShelves(users[0].shelves)
                 added = true;
             }
             
@@ -106,7 +114,10 @@ const MyShelves = () => {
         console.log(newName);
         setNewShelfName(newName);
     };
+    const handleEditInput =(e)=>{
+        setEditedName(e)
 
+    }
     const handleShelfToggle = (shelf) => {
         setSelectedShelves((prevSelected) => {
             if (prevSelected.includes(shelf)) {
@@ -116,13 +127,43 @@ const MyShelves = () => {
             }
         });
     };
+    const handleDeleteShelf = (shelfName) => {
+        // Implement logic to delete the shelf with the given ID
+        users[0].shelves = currentUser.shelves.filter((shelf) => shelf.name !== shelfName);
+        setUserShelves(users[0].shelves)
+    };
+    const handleEditSubmit = (oldShelfName) => {
+        // Implement logic to edit the name of the shelf with the given ID
+        const updatedShelves = userShelves.map((shelf, index) => {
+            if (shelf.name === oldShelfName) {
+                if (editedName === "")
+                    shelf.name = "Shelf "+ index;
+                else
+                    shelf.name= editedName;
+                setEditedName("")
+                return shelf
+            } else {
+                return shelf;
+            }
+        });
+        users[0].shelves = updatedShelves;
+        setUserShelves( users[0].shelves)
+        setSuccess(true)
+        setTimeout(() => {
+            handleEditClose();
+            setSuccess(false);
+            setEditedName("");
+        }, 1000); //
+        // Optionally, you can also update the state or trigger a re-render
+    };
 
     return (
         <div className="my-shelves-container">
             <h1 className="shelves-heading">My Shelves</h1>
             <div className="shelves-container">
-                {userShelves.map((shelf) => (
-                    <div className="shelf-indv-container">
+                {userShelves.map((shelf, index) => {
+                    return (
+                    <div  key={index} className="shelf-indv-container">
                     <Link
                         key={shelf.id}
                         className="shelf"
@@ -131,8 +172,49 @@ const MyShelves = () => {
                     >
                         <h3 className="shelf-name">{shelf.name}</h3>
                     </Link>
+                        <div>
+                            <DeleteIcon className={"delete-icon"} onClick={()=> handleDeleteShelf(shelf.name)}/>
+                            <EditIcon className={"edit-icon"} onClick={handleEditOpen}/>
+                            <Modal
+                                open={editOpen}
+                                onClose={() => handleEditClose()}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                            >
+                                <Box className={"modal-box"}>
+                                    <Typography
+                                        className="modal-modal-title"
+                                        variant="h6"
+                                        component="h2"
+                                    >
+                                        Update Shelf
+                                        <CloseIcon className="close-icon-details" onClick={handleEditClose}></CloseIcon>
+
+                                    </Typography>
+
+                                    <div className="indv_add_shelf_container">
+                                        <input
+                                            className="indv_new_shelf_input"
+                                            type="text"
+                                            placeholder="New Name"
+                                            value={editedName}
+                                            onChange={(e) =>
+                                                handleEditInput(e.target.value)
+                                            }
+                                        />
+                                    </div>
+                                    <button
+                                        className={`indv_submit_button${success ? "_success" : ""
+                                        }`}
+                                        onClick={()=>handleEditSubmit(shelf.name)}
+                                    >
+                                        {success ? "Saved" : "Save"}
+                                    </button>
+                                </Box>
+                            </Modal>
+                        </div>
                     </div>
-                ))}
+                )})}
             </div>
 
             <div className="indv_add_to_shelf">
